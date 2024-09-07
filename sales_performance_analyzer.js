3cfb1f3166150d88f7325f1732a0b20295bc3313
ZhengranHuang <Zhengran@usf.edu>
2024-09-07 18:06:53 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -1,7 +1,21 @@
 //Task1: Create a Function to Calculate Average Sales
-let sales = [200,100,100,200]
+let sales = [10000,10000,5000]
 function calculateAverageSales(sales){
     const totalSales = sales.reduce((sum,number) => sum + number);
     return totalSales / sales.length;
 }
 console.log("AverageSales:$", calculateAverageSales(sales))
+
+//Task2: Create a Function to Determine Performance Rating
+function determinePerformanceRating(sales){
+    if(calculateAverageSales(sales)>10000){
+        return "Excellent";
+    } else if(calculateAverageSales(sales)>=7000){
+        return "Good";
+    } else if(calculateAverageSales(sales)>=4000){
+            return "Satisfactory";
+    } else if(calculateAverageSales(sales)<4000){
+        return "Needs Improvement";
+    }}
+    console.log("PerformanceRating:", determinePerformanceRating(sales))
+    
