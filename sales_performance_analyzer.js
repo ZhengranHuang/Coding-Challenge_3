e4379dd9dda787419664e921a087e85831400a69
ZhengranHuang <Zhengran@usf.edu>
2024-09-08 17:57:57 -0400

    "Develop findTopAndBottomPerformers function"

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -1,5 +1,5 @@
 //Task1: Create a Function to Calculate Average Sales
-let sales = [10000,10000,5000]
+let sales = [0,0,0]
 function calculateAverageSales(sales){
     const totalSales = sales.reduce((sum,number) => sum + number);
     return totalSales / sales.length;
@@ -18,4 +18,33 @@ function determinePerformanceRating(sales){
         return "Needs Improvement";
     }}
     console.log("PerformanceRating:", determinePerformanceRating(sales))
+
+    //Task3:Create a Function to Identify Top and Bottom Performers
+    const salesData = [
+
+        { name: 'Alice', sales: [12000, 15000, 13000] },
+        
+        { name: 'Bob', sales: [7000, 6000, 7500] },
+        
+        { name: 'Charlie', sales: [3000, 4000, 3500] },
+        
+        { name: 'Diana', sales: [9000, 8500, 9200] },
+    ]
+
+    function findTopAndBottomPerformers(salesData){
+        return salesData.reduce((result, salesperson) => {
+            const totalSales = salesperson.sales.reduce((sum,sale) => sum + sale, 0);
+            if (!result. topPerformer || totalSales > result. topPerformer. totalSales){
+                result.topPerformer = {name: salesperson.name, totalSales};
+             if (!result. bottomPerformer || totalSales < result. bottomPerformer. totalSales){
+                result.bottomPerformer = {name: salesperson.name, totalSales};
+            }
+        return result;
+    }
+    })}
+    const result = findTopAndBottomPerformers(salesData);
+    console.log(result)
+
     
+
+
